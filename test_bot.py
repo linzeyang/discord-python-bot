c65930b7e88b17c577588369e19f6dc92e8860e6
Zeyang Lin <4020306+linzeyang@users.noreply.github.com>
2020-04-26 22:01:09 +0100

    pytest with asyncio

---
+++ test_bot.py
@@ -0,0 +1,12 @@
+import asyncio
+
+import pytest
+
+# Mark entire modules or classes with this asyncio marker.
+# Only test coroutines will be affecte.
+pytestmark = pytest.mark.asyncio
+
+
+async def test_example():
+    await asyncio.sleep(1.0)
+    assert True
